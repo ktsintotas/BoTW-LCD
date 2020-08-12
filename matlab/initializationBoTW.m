c145fdc2d797855840b3b5a6fd0dd11d39aeb487
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 20:07:23 +0300

    Update initializationBoTW.m

--- matlab/initializationBoTW.m
+++ matlab/initializationBoTW.m
@@ -1,29 +1,33 @@
 % 
 
-% Copyright 2019, Konstantinos A. Tsintotas
+% Copyright 2020, Konstantinos A. Tsintotas
 % ktsintot@pme.duth.gr
 %
-% This file is part of HMM-BoTW framework for visual loop closure detection
+% This file is part of BoTW-LCD framework for visual loop closure detection
 %
-% HMM-BoTW framework is free software: you can redistribute 
+% BoTW-LCD framework is free software: you can redistribute 
 % it and/or modify it under the terms of the MIT License as 
 % published by the corresponding authors.
 %  
-% HMM-BoTW pipeline is distributed in the hope that it will be 
+% BoTW-LCD pipeline is distributed in the hope that it will be 
 % useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % MIT License for more details. <https://opensource.org/licenses/MIT>
 
-function BoTW = initializationBoTW(visualData)
+function BoTW = initializationBoTW(visualData, params)
         
         % the visual dictionary
-        BoTW.bagOfTrackedWords = single([]);
+        BoTW.bagOfTrackedWords = single(zeros(100000, params.incomingVisualData.descriptorDimension));
         % tracked words' indexing
-        BoTW.twIndex = int16([]);
+        BoTW.twIndex = uint16(zeros(100000, 3));
+        % tracked words' location indexing
+        BoTW.twLocationIndex = false(100000, size(visualData.inputImage, 2));
+        % the deteled words
+        BoTW.deleted = uint16(0);
         % lamda is counting the number of tracked words belonging to the traversed location
-        BoTW.lamda = zeros(1, size(visualData.inputImage, 2), 'int16');
+        BoTW.lamda = zeros(1, size(visualData.inputImage, 2), 'uint16');
         % maximum active point variable for the query process
-        BoTW.maximumActivePoint = zeros(1, size(visualData.inputImage, 2), 'int16');
+        BoTW.maximumActivePoint = zeros(1, size(visualData.inputImage, 2), 'uint16');
         % query points
         BoTW.queryPoints = cell(1, visualData.imagesLoaded);
         % query descriptors

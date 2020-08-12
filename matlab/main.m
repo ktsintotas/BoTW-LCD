8a1d90a0e43ee9a8fa570564f0fc0872613ed792
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 19:55:17 +0300

    Update main.m

--- matlab/main.m
+++ matlab/main.m
@@ -1,15 +1,15 @@
 % 
 
-% Copyright 2019, Konstantinos A. Tsintotas
+% Copyright 2020, Konstantinos A. Tsintotas
 % ktsintot@pme.duth.gr
 %
-% This file is part of HMM-BoTW framework for visual loop closure detection
+% This file is part of BoTW-LCD framework for visual loop closure detection
 %
-% HMM-BoTW framework is free software: you can redistribute 
+% BoTW-LCD framework is free software: you can redistribute 
 % it and/or modify it under the terms of the MIT License as 
 % published by the corresponding authors.
 %  
-% HMM-BoTW pipeline is distributed in the hope that it will be 
+% BoTW-LCD pipeline is distributed in the hope that it will be 
 % useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % MIT License for more details. <https://opensource.org/licenses/MIT>
@@ -24,12 +24,12 @@ params = parametersDefinition();
 % extraction of visual sensory information
 [visualData, timer] = incomingVisualData(params, dataPath, dataFormat);
 % dataset's frame rate definition
-visualData.frameRate = 10; 
+visualData.frameRate = %; 
 % timers memory allocation
 timer = timersInitialization(visualData, timer);
-% 1) the vocabulary build
+% 1) trajectory mapping
 [BoTW, timer] = buildingDatabase(visualData, params, timer);
-% 2)  the query procedure
+% 2) the query procedure
 [matches, HMMresults, iBoTW, timer] = queryingDatabaseHMM(params, visualData, BoTW, timer);
 % method's evaluation
 close all;
